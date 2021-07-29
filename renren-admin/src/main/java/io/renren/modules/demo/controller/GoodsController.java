defeebd93110f95c78220886cb0dc89e29afa3c4
xhm993502807 <993502807@qq.com>
2021-07-29 10:55:03 +0800

    hot-fix commit 2

--- renren-admin/src/main/java/io/renren/modules/demo/controller/GoodsController.java
+++ renren-admin/src/main/java/io/renren/modules/demo/controller/GoodsController.java
@@ -38,8 +38,8 @@ public class GoodsController {
     @RequestMapping("/list")
     @RequiresPermissions("demo:goods:list")
     public R list(@RequestParam Map<String, Object> params){
-        System.out.println("----------------hot fix");
-        System.out.println("----------------hot fix");
+        System.out.println("----------------hot fix88888");
+        System.out.println("----------------hot fix888888");
         PageUtils page = goodsService.queryPage(params);
 
         return R.ok().put("page", page);
